c70507ed406723a5779d4b04745413e4e11bf423
Wang Yafang <wangyafang0219@163.com>
2015-05-25 14:28:24 +0800

    homework for JUnit

---
+++ src/TestCalculate.java
@@ -0,0 +1,56 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Ignore;
+
+
+public class TestCalculate {
+
+	private static Calculate calculator = new Calculate();
+
+
+    @Before
+    public void setUp() throws Exception {
+        calculator.clear();
+    }
+
+ 
+
+    @Test
+    public void testAdd() {
+        calculator.add(2);
+        calculator.add(3);
+        assertEquals(5, calculator.getResult());
+    }
+
+ 
+
+    @Test
+    public void testSubstract() {
+        calculator.add(10);
+        calculator.substract(2);
+        assertEquals(8, calculator.getResult());
+    }
+
+ 
+
+    @Ignore("Multiply() Not yet implemented")
+
+    
+    @Test
+    public void testMultiply() {
+
+    }
+
+ 
+
+    @Test
+
+    public void testDivide() {
+        calculator.add(8);
+        calculator.divide(2);
+        assertEquals(4, calculator.getResult());
+
+    }
+}
